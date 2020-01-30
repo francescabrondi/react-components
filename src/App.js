87fc2863186a166a4581542251e5024ec8e1ba56
Francesca <francescabrondi@gmail.com>
2020-01-30 14:42:32 +0100

    Quest 03

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
 import React from "react";
-import Contact from "./components/Contact";
+import ContactList from "./components/ContactList";
 
 function App() {
   const users = [
     {
       id: 1,
-      name: "Nathaniel",
+      name: "Erika",
       online: false,
-      avatar: "https://randomuser.me/api/portraits/men/47.jpg"
+      avatar: "https://randomuser.me/api/portraits/women/40.jpg"
     },
     {
       id: 2,
-      name: "Francesco",
+      name: "Melanie",
       online: true,
-      avatar: "https://randomuser.me/api/portraits/men/1.jpg"
+      avatar: "https://randomuser.me/api/portraits/women/52.jpg"
     },
     {
       id: 3,
-      name: "Mai na Joy",
+      name: "Heather",
       online: false,
-      avatar: "https://randomuser.me/api/portraits/women/47.jpg"
+      avatar: "https://randomuser.me/api/portraits/women/22.jpg"
+    },
+    {
+      id: 4,
+      name: "Clyde",
+      online: true,
+      avatar: "https://randomuser.me/api/portraits/men/91.jpg"
+    },
+    {
+      id: 5,
+      name: "Hector",
+      online: false,
+      avatar: "https://randomuser.me/api/portraits/men/20.jpg"
     }
   ];
 
   return (
     <div className="App">
-      {users.map(users => (
-        <Contact
-          name={users.name}
-          online={users.online}
-          avatar={users.avatar}
-        />
-      ))}
+      <ContactList users={users} />
     </div>
   );
 }
